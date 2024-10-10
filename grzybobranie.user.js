b194e2669f141e473ea714f64fc8824028fcdd3c
Nolif1 <dawidos1505@wp.pl>
2024-10-10 14:00:48 +0200

    Update grzybobranie.user.js

--- grzybobranie.user.js
+++ grzybobranie.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GRZYBY
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      4.0
 // @description  Informuje o pojawieniu się grzybów na Discordzie
 // @author       Nolifequ
 // @icon         https://i.ibb.co/NrsXPQs/grzyb20-out-la.png
