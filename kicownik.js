dd0c5163ae5921f99b218eae27f32bcf8639ab21
Nolif1 <dawidos1505@wp.pl>
2024-11-30 00:11:02 +0100

    Update kicownik.js

--- kicownik.js
+++ kicownik.js
@@ -58,7 +58,7 @@
     }
 
     function displayPopup(nick, npc, map) {
-        if (npc.nick === 'Zabójczy Królik' && notificationCount < maxNotifications) {
+        if (npc.nick === 'Fiodor' && notificationCount < maxNotifications) {
             const pushbulletTitle = 'Zabójczy Królik (70lvl)';
             const pushbulletBody = `${map.name} (${npc.x}, ${npc.y})`;
             const discordMessage = `@here Zabójczy Królik (70lvl) ${map.name} (${npc.x}, ${npc.y})`;
@@ -106,5 +106,5 @@
 
     start();
 })([
-    'Zabójczy Królik'
+    'Fiodor'
 ]);
